04eea7b85a9457c13b3ca8599635db6015f1d610
Bing <Bing@xobo.org>
2018-08-16 10:35:49 +0800

    add skins

---
+++ src/main/resources/dorado/skins/xoboblue/support.js
@@ -0,0 +1,6 @@
+$setting["widget.MessageBox.defaultModalType"] = "dark";
+$setting["widget.ListBox.defaultRowHeight"] = 26;
+$setting["widget.Grid.defaultRowHeight"] = 26;
+$setting["widget.Tree.defaultRowHeight"] = 26;
+$setting["widget.TextEditor.triggerWidth"] = 20;
+$setting["common.taskIndicator.daemon.type"] = "icon";
